c2a8ba4dd30e7b59ca9874ef70c49e5db86c1268
Simon-Montoya <159563090+Simon-Montoya@users.noreply.github.com>
2024-11-19 10:02:11 -0500

    pruebass

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase/firebaseConfig"; // Importa la configuración de Firebase
-import styles from "./card.module.css";
+
 
 function CardComponent() {
   const [properties, setProperties] = useState([]);
@@ -9,12 +7,12 @@ function CardComponent() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const querySnapshot = await getDocs(collection(db, "properties")); // Cambia "properties" por el nombre de tu colección
+        const querySnapshot = await getDocs(collection(db, "CasasBogota"));
         const propertiesData = querySnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
-        setProperties(propertiesData); // Guarda los datos en el estado
+        setProperties(propertiesData); 
       } catch (error) {
         console.error("Error fetching properties: ", error);
       }
@@ -27,11 +25,11 @@ function CardComponent() {
     <div>
       {properties.map((property) => (
         <div key={property.id} className={styles.container}>
-          <h2>{property["Título"] || "Título no disponible"}</h2>
-          <p>Precio: {property["Precio"] || "No disponible"}</p>
-          <p>Habitaciones: {property["Habitaciones"] || "No disponible"}</p>
-          <p>Baños: {property["Baños"] || "No disponible"}</p>
-          <p>Área (m²): {property["Área (m²)"] || "No disponible"}</p>
+          {property["Título"] && <h2>{property["Título"]}</h2>}
+          {property["Precio"] && <p>Precio: {property["Precio"]}</p>}
+          {property["Habitaciones"] && <p>Habitaciones: {property["Habitaciones"]}</p>}
+          {property["Baños"] && <p>Baños: {property["Baños"]}</p>}
+          {property["Área (m²)"] && <p>Área (m²): {property["Área (m²)"]}</p>}
         </div>
       ))}
     </div>

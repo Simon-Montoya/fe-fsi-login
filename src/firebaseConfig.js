b5d674af5fa749acc5b36a952573af3adb57c147
Simon-Montoya <159563090+Simon-Montoya@users.noreply.github.com>
2024-11-19 13:58:29 -0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,8 +3,8 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "922891554048",
-  authDomain: "922891554048",
+  apiKey: "AIzaSyA9dy1F39qVUOFtvc4Of1ZLKckwck7Zyss",
+  authDomain: "https://console.firebase.google.com/project/casasbogota-2a61d/database/casasbogota-2a61d-default-rtdb/data/~2F",
   projectId: "casasbogota-2a61d",
   storageBucket: "922891554048",
   messagingSenderId: "922891554048",
